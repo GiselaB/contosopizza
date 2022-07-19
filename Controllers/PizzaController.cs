e3ebef065f6dca9cc41ab78b0f4ce262c20fb6ea
GiselaB <giselabairros@gmail.com>
2022-07-18 21:11:45 -0300

    fixing action name on post

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -31,7 +31,7 @@ public class PizzaController : ControllerBase
     public IActionResult Create(Pizza pizza)
     {
         PizzaService.Add(pizza);
-        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
     }
 
     [HttpPut("{id}")]
